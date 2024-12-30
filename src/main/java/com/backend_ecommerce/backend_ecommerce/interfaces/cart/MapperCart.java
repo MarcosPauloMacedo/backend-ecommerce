4b7b7f6898682002f421eea9b53340349f3ec6e0
Marcos Paulo Macedo <102096687+MarcosPauloMacedo@users.noreply.github.com>
2024-12-30 16:53:39 -0300

    adicionando método para converter ID de usuário em entidade User no MapperCart

--- src/main/java/com/backend_ecommerce/backend_ecommerce/interfaces/cart/MapperCart.java
+++ src/main/java/com/backend_ecommerce/backend_ecommerce/interfaces/cart/MapperCart.java
@@ -2,7 +2,10 @@ package com.backend_ecommerce.backend_ecommerce.interfaces.cart;
 
 import com.backend_ecommerce.backend_ecommerce.interfaces.shared.Mapper;
 import com.backend_ecommerce.backend_ecommerce.models.entity.Cart;
+import com.backend_ecommerce.backend_ecommerce.models.entity.User;
 import com.backend_ecommerce.backend_ecommerce.models.request.CartRequest;
 import com.backend_ecommerce.backend_ecommerce.models.response.CartResponse;
 
-public interface MapperCart extends Mapper<CartRequest,CartResponse,Cart> {}
+public interface MapperCart extends Mapper<CartRequest,CartResponse,Cart> {
+    User toUserEntity(Long userId);
+}
