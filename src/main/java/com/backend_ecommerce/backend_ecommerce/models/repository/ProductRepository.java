cb2683265f9705543c5dfa0f3baff69f88c42c4e
Marcos Paulo Macedo <102096687+MarcosPauloMacedo@users.noreply.github.com>
2024-12-30 23:54:58 -0300

    adicionando suporte a especificações JPA no repositório de produtos

--- src/main/java/com/backend_ecommerce/backend_ecommerce/models/repository/ProductRepository.java
+++ src/main/java/com/backend_ecommerce/backend_ecommerce/models/repository/ProductRepository.java
@@ -1,9 +1,11 @@
 package com.backend_ecommerce.backend_ecommerce.models.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import com.backend_ecommerce.backend_ecommerce.models.entity.Product;
 
 @Repository
-public interface ProductRepository extends JpaRepository<Product, Long> {}
+public interface ProductRepository extends JpaRepository<Product, Long>, 
+JpaSpecificationExecutor<Product> {}
