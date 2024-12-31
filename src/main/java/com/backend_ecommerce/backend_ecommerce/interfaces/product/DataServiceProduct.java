3c1dc37048890a795682ea7bd71527d2fce3fc34
Marcos Paulo Macedo <102096687+MarcosPauloMacedo@users.noreply.github.com>
2024-12-30 23:56:26 -0300

    adicionando métodos de seleção com filtragem para produtos e usuários nas interfaces DataService

--- src/main/java/com/backend_ecommerce/backend_ecommerce/interfaces/product/DataServiceProduct.java
+++ src/main/java/com/backend_ecommerce/backend_ecommerce/interfaces/product/DataServiceProduct.java
@@ -2,6 +2,12 @@ package com.backend_ecommerce.backend_ecommerce.interfaces.product;
 
 import com.backend_ecommerce.backend_ecommerce.interfaces.shared.DataService;
 import com.backend_ecommerce.backend_ecommerce.models.request.ProductRequest;
+import com.backend_ecommerce.backend_ecommerce.models.response.PageResponse;
 import com.backend_ecommerce.backend_ecommerce.models.response.ProductResponse;
+import com.backend_ecommerce.backend_ecommerce.models.utils.PageFilter;
+import com.backend_ecommerce.backend_ecommerce.models.utils.PageProductFilter;
 
-public interface DataServiceProduct extends DataService <ProductResponse,ProductRequest> {}
+public interface DataServiceProduct extends DataService <ProductResponse,ProductRequest> {
+    PageResponse<ProductResponse> selectAll(PageFilter pageFilter,
+    PageProductFilter pageProductFilter);
+}
--- src/main/java/com/backend_ecommerce/backend_ecommerce/interfaces/user/DataServiceUser.java
+++ src/main/java/com/backend_ecommerce/backend_ecommerce/interfaces/user/DataServiceUser.java
@@ -2,6 +2,10 @@ package com.backend_ecommerce.backend_ecommerce.interfaces.user;
 
 import com.backend_ecommerce.backend_ecommerce.interfaces.shared.DataService;
 import com.backend_ecommerce.backend_ecommerce.models.request.UserRequest;
+import com.backend_ecommerce.backend_ecommerce.models.response.PageResponse;
 import com.backend_ecommerce.backend_ecommerce.models.response.UserResponse;
+import com.backend_ecommerce.backend_ecommerce.models.utils.PageFilter;
 
-public interface DataServiceUser extends DataService<UserResponse, UserRequest> {}
+public interface DataServiceUser extends DataService<UserResponse, UserRequest> {
+    PageResponse<UserResponse> selectAll(PageFilter pageFilter);
+}
