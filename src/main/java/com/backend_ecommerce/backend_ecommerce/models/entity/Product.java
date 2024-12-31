1cdb838cc86e40dd13db5cd42db9ab6e10444bb7
Marcos Paulo Macedo <102096687+MarcosPauloMacedo@users.noreply.github.com>
2024-12-30 23:55:12 -0300

    adicionando suporte a enum Categories na entidade Product

--- src/main/java/com/backend_ecommerce/backend_ecommerce/models/entity/Product.java
+++ src/main/java/com/backend_ecommerce/backend_ecommerce/models/entity/Product.java
@@ -1,6 +1,10 @@
 package com.backend_ecommerce.backend_ecommerce.models.entity;
 
+import com.backend_ecommerce.backend_ecommerce.models.utils.Categories;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -17,6 +21,8 @@ public class Product {
     private String description;
     private double price;
     private int stock;
-    private String category;
+
+    @Enumerated(EnumType.STRING)
+    private Categories category;
     private String image;
 }
