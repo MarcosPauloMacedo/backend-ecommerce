c26a57cafecc8a52347b92bf080081d66e0ec5fc
Marcos Paulo Macedo <102096687+MarcosPauloMacedo@users.noreply.github.com>
2024-12-30 23:56:06 -0300

    adicionando métodos para seleção e cálculo de preço total no DataServiceCart

--- src/main/java/com/backend_ecommerce/backend_ecommerce/interfaces/cart/DataServiceCart.java
+++ src/main/java/com/backend_ecommerce/backend_ecommerce/interfaces/cart/DataServiceCart.java
@@ -3,5 +3,10 @@ package com.backend_ecommerce.backend_ecommerce.interfaces.cart;
 import com.backend_ecommerce.backend_ecommerce.interfaces.shared.DataService;
 import com.backend_ecommerce.backend_ecommerce.models.request.CartRequest;
 import com.backend_ecommerce.backend_ecommerce.models.response.CartResponse;
+import com.backend_ecommerce.backend_ecommerce.models.response.PageResponse;
+import com.backend_ecommerce.backend_ecommerce.models.utils.PageFilter;
 
-public interface DataServiceCart extends DataService<CartResponse, CartRequest> {}
+public interface DataServiceCart extends DataService<CartResponse, CartRequest> {
+    PageResponse<CartResponse> selectAll(PageFilter pageFilter);
+    String calculateTotalPriceCartByUser(Long userId);
+}
