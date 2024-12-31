63794cc65c731c82683e8cb224342baf125b5801
Marcos Paulo Macedo <102096687+MarcosPauloMacedo@users.noreply.github.com>
2024-12-30 23:51:07 -0300

    adicionando método para buscar carrinhos por usuário no CartRepository

--- src/main/java/com/backend_ecommerce/backend_ecommerce/models/repository/CartRepository.java
+++ src/main/java/com/backend_ecommerce/backend_ecommerce/models/repository/CartRepository.java
@@ -3,5 +3,10 @@ package com.backend_ecommerce.backend_ecommerce.models.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.backend_ecommerce.backend_ecommerce.models.entity.Cart;
+import java.util.List;
+import com.backend_ecommerce.backend_ecommerce.models.entity.User;
 
-public interface CartRepository extends JpaRepository<Cart, Long> {}
+
+public interface CartRepository extends JpaRepository<Cart, Long> {
+    List<Cart> findByUser(User user);
+}
