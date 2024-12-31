45f2bcac512bdfbdfaebb0e23ec6e43f1fae9cde
Marcos Paulo Macedo <102096687+MarcosPauloMacedo@users.noreply.github.com>
2024-12-30 23:55:51 -0300

    removendo método selectAll da interface DataService

--- src/main/java/com/backend_ecommerce/backend_ecommerce/interfaces/shared/DataService.java
+++ src/main/java/com/backend_ecommerce/backend_ecommerce/interfaces/shared/DataService.java
@@ -1,11 +1,8 @@
 package com.backend_ecommerce.backend_ecommerce.interfaces.shared;
 
-import com.backend_ecommerce.backend_ecommerce.models.response.PageResponse;
-
 public interface DataService <Response, Request> {
     public Response save(Request request);
     public Response update(Long id, Request request);
     public Response selectById(Long id);
-    public PageResponse<Response> selectAll(int page, int size);
     public void delete(Long id);
 }
