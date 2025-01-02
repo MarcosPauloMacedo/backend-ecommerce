1dab4e8e5f3d324c64404c66a776f004fc23a3c5
Marcos Paulo Macedo <102096687+MarcosPauloMacedo@users.noreply.github.com>
2025-01-02 17:06:19 -0300

    adicionando interface para seleção de todos os itens no carrinho e cálculo de preço por item

--- src/main/java/com/backend_ecommerce/backend_ecommerce/interfaces/cart/DataServiceCart.java
+++ src/main/java/com/backend_ecommerce/backend_ecommerce/interfaces/cart/DataServiceCart.java
@@ -1,12 +1,12 @@
 package com.backend_ecommerce.backend_ecommerce.interfaces.cart;
 
 import com.backend_ecommerce.backend_ecommerce.interfaces.shared.DataService;
+import com.backend_ecommerce.backend_ecommerce.interfaces.shared.SelectAll;
 import com.backend_ecommerce.backend_ecommerce.models.request.CartRequest;
 import com.backend_ecommerce.backend_ecommerce.models.response.CartResponse;
-import com.backend_ecommerce.backend_ecommerce.models.response.PageResponse;
-import com.backend_ecommerce.backend_ecommerce.models.utils.PageFilter;
 
-public interface DataServiceCart extends DataService<CartResponse, CartRequest> {
-    PageResponse<CartResponse> selectAll(PageFilter pageFilter);
+public interface DataServiceCart extends DataService<CartResponse, CartRequest>,
+SelectAll<CartResponse> {
     String calculateTotalPriceCartByUser(Long userId);
+    String calculatePricePerItemOfCart(Long cartId);
 }
