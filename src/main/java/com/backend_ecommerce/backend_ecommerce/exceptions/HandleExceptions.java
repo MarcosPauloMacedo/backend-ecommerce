da1579995adcff139bf94d7e2c0ea7d4f1bf73fe
Marcos Paulo Macedo <102096687+MarcosPauloMacedo@users.noreply.github.com>
2025-01-03 14:21:47 -0300

    criando pasta com classes responsáveis pelo tratamento de exceções

---
+++ src/main/java/com/backend_ecommerce/backend_ecommerce/exceptions/HandleExceptions.java
@@ -0,0 +1,41 @@
+package com.backend_ecommerce.backend_ecommerce.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+@ControllerAdvice
+public class HandleExceptions {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorDetails> resourceNotFoundException(ResourceNotFoundException ex, 
+    WebRequest request) {
+        
+        ErrorDetails errorDetails = new ErrorDetails();
+
+        var timestamp = java.time.LocalDateTime.now();
+
+        errorDetails.setTimestamp(timestamp);
+        errorDetails.setStatusCode(HttpStatus.NOT_FOUND.value());
+        errorDetails.setMessage(ex.getMessage());
+        errorDetails.setDetails(request.getDescription(false));
+
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorDetails> globalExceptionHandler(Exception ex, WebRequest request) {
+        ErrorDetails errorDetails = new ErrorDetails();
+
+        var timestamp = java.time.LocalDateTime.now();
+
+        errorDetails.setTimestamp(timestamp);
+        errorDetails.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        errorDetails.setMessage(ex.getMessage());
+        errorDetails.setDetails(request.getDescription(false));
+
+        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
