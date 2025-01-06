9c1efed034fcd68f6c3a80b48e469428409eab88
Marcos Paulo Macedo <102096687+MarcosPauloMacedo@users.noreply.github.com>
2025-01-06 14:23:11 -0300

    adicionando anotações EntityGraph para otimizar consultas de OrderItem

--- src/main/java/com/backend_ecommerce/backend_ecommerce/models/repository/OrderItemRepository.java
+++ src/main/java/com/backend_ecommerce/backend_ecommerce/models/repository/OrderItemRepository.java
@@ -4,12 +4,16 @@ import java.util.List;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.backend_ecommerce.backend_ecommerce.models.entity.OrderItem;
 import com.backend_ecommerce.backend_ecommerce.models.entity.Orders;
 
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+    @EntityGraph(attributePaths = {"product"})
     Page<OrderItem> findByOrder(Orders order, Pageable pageable);
+
+    @EntityGraph(attributePaths = {"product"})
     List<OrderItem> findByOrder(Orders order);
 }
