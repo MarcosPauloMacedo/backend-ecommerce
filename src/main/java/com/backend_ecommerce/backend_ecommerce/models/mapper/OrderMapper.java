bf5d31cb5534ec06ca267d03f8f958470094aaa2
Marcos Paulo Macedo <102096687+MarcosPauloMacedo@users.noreply.github.com>
2025-01-06 14:17:02 -0300

    adicionando métodos para conversão de OrderResponse em Orders e criação de pedidos por usuário

--- src/main/java/com/backend_ecommerce/backend_ecommerce/models/mapper/OrderMapper.java
+++ src/main/java/com/backend_ecommerce/backend_ecommerce/models/mapper/OrderMapper.java
@@ -1,5 +1,6 @@
 package com.backend_ecommerce.backend_ecommerce.models.mapper;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.modelmapper.ModelMapper;
@@ -9,9 +10,11 @@ import org.springframework.stereotype.Component;
 
 import com.backend_ecommerce.backend_ecommerce.interfaces.order.MapperOrder;
 import com.backend_ecommerce.backend_ecommerce.models.entity.Orders;
+import com.backend_ecommerce.backend_ecommerce.models.entity.User;
 import com.backend_ecommerce.backend_ecommerce.models.request.OrderRequest;
 import com.backend_ecommerce.backend_ecommerce.models.response.OrderResponse;
 import com.backend_ecommerce.backend_ecommerce.models.response.PageResponse;
+import com.backend_ecommerce.backend_ecommerce.models.utils.OrderStatus;
 
 @Component
 public class OrderMapper implements MapperOrder {
@@ -28,6 +31,26 @@ public class OrderMapper implements MapperOrder {
     public Orders toEntity(Long id, OrderRequest request) {
         Orders order = this.toEntity(request);
         order.setId(id);
+
+        return order;
+    }
+
+    @Override
+    public Orders toEntity(OrderResponse orderResponse) {
+        return modelMapper.map(orderResponse, Orders.class);
+    }
+
+    @Override
+    public Orders toEntityByUser(Long userId) {
+        var createAt = LocalDateTime.now();
+
+        User user = new User();
+        user.setId(userId);
+
+        Orders order = new Orders();
+        order.setUser(user);
+        order.setStatus(OrderStatus.PENDING);
+        order.setCreateAt(createAt);
 
         return order;
     }
