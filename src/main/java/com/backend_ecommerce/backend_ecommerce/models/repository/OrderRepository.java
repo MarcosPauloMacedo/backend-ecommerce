c3d6d34ae8e97a6d251f09f4a2a79d823dd43c14
Marcos Paulo Macedo <102096687+MarcosPauloMacedo@users.noreply.github.com>
2025-01-06 14:18:49 -0300

    adicionando método para buscar e-mail do usuário associado a um pedido

--- src/main/java/com/backend_ecommerce/backend_ecommerce/models/repository/OrderRepository.java
+++ src/main/java/com/backend_ecommerce/backend_ecommerce/models/repository/OrderRepository.java
@@ -3,10 +3,15 @@ package com.backend_ecommerce.backend_ecommerce.models.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.backend_ecommerce.backend_ecommerce.models.entity.Orders;
 import com.backend_ecommerce.backend_ecommerce.models.entity.User;
 
 public interface OrderRepository extends JpaRepository<Orders, Long> {
     List<Orders> findByUser(User user);
+
+    @Query("SELECT o.user.email FROM Orders o WHERE o.id = :id")
+    String findEmailByOrderId(@Param("id") Long id);
 }
