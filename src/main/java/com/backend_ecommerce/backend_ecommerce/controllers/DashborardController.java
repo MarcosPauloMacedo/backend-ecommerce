e69830c0c955e6bef37e2a624a226f149c15c7d1
Marcos Paulo Macedo <102096687+MarcosPauloMacedo@users.noreply.github.com>
2025-01-06 16:25:53 -0300

    adicionando controlador DashborardController para listar produtos com estoque baixo

---
+++ src/main/java/com/backend_ecommerce/backend_ecommerce/controllers/DashborardController.java
@@ -0,0 +1,37 @@
+package com.backend_ecommerce.backend_ecommerce.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.backend_ecommerce.backend_ecommerce.models.response.PageResponse;
+import com.backend_ecommerce.backend_ecommerce.models.response.ProductResponse;
+import com.backend_ecommerce.backend_ecommerce.models.utils.PageFilter;
+import com.backend_ecommerce.backend_ecommerce.services.product.ProductService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@RestController
+@RequestMapping("/dashboard")
+@Tag(name = "Dashboard", description = "Gerenciamento do dashboard")
+public class DashborardController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping("/low-stock")
+    @Operation(summary = "Listar produtos com estoque baixo")
+    public PageResponse<ProductResponse> selectByLowStock(
+        @RequestParam(defaultValue = "5") int quantity,
+        @RequestParam(defaultValue = "0") int page,
+        @RequestParam(defaultValue = "10") int size,
+        @RequestParam(defaultValue = "ASC") String sortOrder,
+        @RequestParam(defaultValue = "id") String sortField
+    ) { 
+        var pageFilter = new PageFilter(page, size, sortOrder, sortField);
+        return productService.selectByLowStock(quantity,pageFilter);
+    }
+}
