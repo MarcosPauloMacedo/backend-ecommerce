3f18a34936162c607ccaae750183f24636edc542
Marcos Paulo Macedo <102096687+MarcosPauloMacedo@users.noreply.github.com>
2025-01-09 00:08:34 -0300

    adicionando configuração de segurança para autenticação JWT e integração com UserService

--- src/main/java/com/backend_ecommerce/backend_ecommerce/config/SecurityConfig.java
+++ src/main/java/com/backend_ecommerce/backend_ecommerce/config/SecurityConfig.java
@@ -1,27 +1,58 @@
 package com.backend_ecommerce.backend_ecommerce.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.backend_ecommerce.backend_ecommerce.services.user.UserService;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Lazy
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Bean
+    AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        var authenticationManagerBuilder = http.getSharedObject(
+            AuthenticationManagerBuilder.class
+        );
+
+        authenticationManagerBuilder.userDetailsService(userService)
+        .passwordEncoder(passwordEncoder());
+
+        return authenticationManagerBuilder.build();
+    }
+
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-            .csrf((csrf) -> csrf.disable())
-            .httpBasic(Customizer.withDefaults())
-            .formLogin(Customizer.withDefaults())
+            .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(authorize -> authorize
+                .requestMatchers("/auth/login").permitAll()
                 .anyRequest().authenticated()
+            )
+            .sessionManagement(session -> session
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             );
+
+        http.addFilterBefore(jwtAuthenticationFilter, 
+        UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
