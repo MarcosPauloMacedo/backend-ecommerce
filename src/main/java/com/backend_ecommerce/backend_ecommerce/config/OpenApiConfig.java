eed21bb40c397cf903b132b21fc9cfc779ce7821
Marcos Paulo Macedo <102096687+MarcosPauloMacedo@users.noreply.github.com>
2025-01-09 01:10:04 -0300

    adicionando configuração de segurança para autenticação JWT na documentação OpenAPI

--- src/main/java/com/backend_ecommerce/backend_ecommerce/config/OpenApiConfig.java
+++ src/main/java/com/backend_ecommerce/backend_ecommerce/config/OpenApiConfig.java
@@ -1,7 +1,11 @@
 package com.backend_ecommerce.backend_ecommerce.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,6 +20,15 @@ public class OpenApiConfig {
             .title("E-commerce API")
             .version("1.0.0")
             .description("API para gerenciamento de produtos e pedidos em um sistema de e-commerce")
+        )
+        .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+        .components(
+            new Components()
+                .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                    .type(SecurityScheme.Type.HTTP)
+                    .scheme("bearer")
+                    .bearerFormat("JWT")
+                )
         );
     }
 }
