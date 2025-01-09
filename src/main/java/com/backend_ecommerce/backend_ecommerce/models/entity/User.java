652fa870a7c6e086fece803b49d11ef1dd1182aa
Marcos Paulo Macedo <102096687+MarcosPauloMacedo@users.noreply.github.com>
2025-01-09 00:08:16 -0300

    implementando a interface UserDetails na entidade User para integração com o Spring Security

--- src/main/java/com/backend_ecommerce/backend_ecommerce/models/entity/User.java
+++ src/main/java/com/backend_ecommerce/backend_ecommerce/models/entity/User.java
@@ -1,5 +1,11 @@
 package com.backend_ecommerce.backend_ecommerce.models.entity;
 
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import com.backend_ecommerce.backend_ecommerce.models.utils.Roles;
 
 import jakarta.persistence.Entity;
@@ -12,7 +18,7 @@ import lombok.Data;
 
 @Data
 @Entity
-public class User {
+public class User implements UserDetails {
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -24,4 +30,38 @@ public class User {
 
     @Enumerated(EnumType.STRING)
     private Roles role;
+
+    @Override 
+    public Collection<? extends  GrantedAuthority > getAuthorities() { 
+        return List.of(); 
+    } 
+
+    public String getPassword () { 
+        return password; 
+    } 
+
+    @Override 
+    public String getUsername () { 
+        return email; 
+    } 
+
+    @Override 
+    public boolean isAccountNonExpired () { 
+        return true ; 
+    } 
+
+    @Override 
+    public boolean isAccountNonLocked () { 
+        return true ; 
+    } 
+
+    @Override 
+    public boolean isCredentialsNonExpired () { 
+        return true ; 
+    } 
+
+    @Override 
+    public boolean isEnabled () { 
+        return true ; 
+    } 
 }
