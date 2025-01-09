ff3b6cc5fe702014050c64838ad2f811c3d45a17
Marcos Paulo Macedo <102096687+MarcosPauloMacedo@users.noreply.github.com>
2025-01-09 00:07:14 -0300

    adicionando filtro de autenticação JWT para validação de tokens

---
+++ src/main/java/com/backend_ecommerce/backend_ecommerce/config/JwtAuthenticationFilter.java
@@ -0,0 +1,84 @@
+package com.backend_ecommerce.backend_ecommerce.config;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.backend_ecommerce.backend_ecommerce.services.auth.JwtService;
+import com.backend_ecommerce.backend_ecommerce.services.user.UserService;
+
+import io.jsonwebtoken.ExpiredJwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private JwtService jwtService;
+
+    @Lazy
+    @Autowired
+    private UserService userService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, 
+    HttpServletResponse response, FilterChain chain)
+        throws ServletException, IOException {
+
+        final String requestTokenHeader = request.getHeader("Authorization");
+
+        String username = null;
+        String jwtToken = null;
+
+        // JWT Token está no formato "Bearer token". Remova a palavra "Bearer" e pegue apenas o Token
+        if (requestTokenHeader != null && requestTokenHeader.startsWith("Bearer ")) {
+            jwtToken = requestTokenHeader.substring(7);
+
+            try {
+                username = jwtService.extractUsername(jwtToken);
+
+            } catch (IllegalArgumentException e) {
+                throw new IllegalArgumentException("Não é possível obter o token JWT");
+
+            } catch (ExpiredJwtException e) {
+                throw new ExpiredJwtException(null, null, "Token JWT expirado");
+
+            }
+
+        } else {
+            logger.warn("JWT Token não está presente ou não começa com Bearer String");
+            throw new IllegalArgumentException("JWT Token não está presente ou não começa com Bearer String");
+        }
+
+        // Depois de obter o token, valide-o.
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = userService.loadUserByUsername(username);
+
+            // Se o token for válido, configure a autenticação manualmente
+            if (jwtService.validateToken(jwtToken, userDetails.getUsername())) {
+                var usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
+                    userDetails, null, userDetails.getAuthorities()
+                );
+
+                usernamePasswordAuthenticationToken.setDetails(
+                    new WebAuthenticationDetailsSource().buildDetails(request)
+                );
+
+                // Após definir a autenticação no contexto, especificamos que o usuário está autenticado.
+                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+            }
+        }
+        
+        chain.doFilter(request, response);
+    }
+}
