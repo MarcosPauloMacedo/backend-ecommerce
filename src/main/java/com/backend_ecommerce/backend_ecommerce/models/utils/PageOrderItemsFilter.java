875d7fee9118a241a34feae12ed40ec085d310a2
Marcos Paulo Macedo <102096687+MarcosPauloMacedo@users.noreply.github.com>
2025-01-09 19:20:30 -0300

    adicionando classe PageOrderItemsFilter para suporte a filtragem de itens de pedido por categoria

---
+++ src/main/java/com/backend_ecommerce/backend_ecommerce/models/utils/PageOrderItemsFilter.java
@@ -0,0 +1,12 @@
+package com.backend_ecommerce.backend_ecommerce.models.utils;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PageOrderItemsFilter {
+    private String category;
+}
