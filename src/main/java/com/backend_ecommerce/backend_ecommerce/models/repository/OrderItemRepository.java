4013b81f48cb9cd6c1c702656a33f054c049a979
Marcos Paulo Macedo <102096687+MarcosPauloMacedo@users.noreply.github.com>
2025-01-09 19:20:47 -0300

    adicionando suporte a especificações JPA no repositório OrderItemRepository

--- src/main/java/com/backend_ecommerce/backend_ecommerce/models/repository/OrderItemRepository.java
+++ src/main/java/com/backend_ecommerce/backend_ecommerce/models/repository/OrderItemRepository.java
@@ -6,11 +6,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import com.backend_ecommerce.backend_ecommerce.models.entity.OrderItem;
 import com.backend_ecommerce.backend_ecommerce.models.entity.Orders;
 
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long>,
+JpaSpecificationExecutor<OrderItem> {
     @EntityGraph(attributePaths = {"product"})
     Page<OrderItem> findByOrder(Orders order, Pageable pageable);
 
