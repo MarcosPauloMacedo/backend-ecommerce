35a62be3ee0473a574262068e14a843b27bc9103
Marcos Paulo Macedo <102096687+MarcosPauloMacedo@users.noreply.github.com>
2025-01-10 17:41:50 -0300

    adicionando arquivo .env com configurações de banco de dados, email e JWT; atualizando UserInitializer para usar variáveis de ambiente

--- src/main/java/com/backend_ecommerce/backend_ecommerce/config/UserInitializer.java
+++ src/main/java/com/backend_ecommerce/backend_ecommerce/config/UserInitializer.java
@@ -1,6 +1,7 @@
 package com.backend_ecommerce.backend_ecommerce.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,21 +13,27 @@ import com.backend_ecommerce.backend_ecommerce.services.user.UserService;
 @Configuration
 public class UserInitializer {
 
+    @Value("${USER_INITIALIZER_EMAIL}")
+    private String userInitializerEmail;
+
+    @Value("${USER_INITIALIZER_PASSWORD}")
+    private String userInitializerPassword;
+
     @Autowired
     private UserService userService;
 
     @Bean
     CommandLineRunner init() {
         return args -> {
-            if (userService.existsByEmail("admin@example.com")) {
+            if (userService.existsByEmail(userInitializerEmail)) {
                 return;
             }
 
             UserRequest user = new UserRequest();
-            user.setEmail("admin@example.com");
-            user.setPassword("Senha,123");
+            user.setEmail(userInitializerEmail);
+            user.setPassword(userInitializerPassword);
             user.setName("Admin");
-            user.setRole(Roles.ADMIN);
+            user.setRole(Roles.ROLE_ADMIN);
 
             userService.save(user);
         };
