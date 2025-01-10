21d5b6cf10ba63254c047e557cfbe83f21f5fef0
Marcos Paulo Macedo <102096687+MarcosPauloMacedo@users.noreply.github.com>
2025-01-10 17:40:33 -0300

    renomeando constantes de roles para incluir o prefixo 'ROLE'

--- src/main/java/com/backend_ecommerce/backend_ecommerce/models/utils/Roles.java
+++ src/main/java/com/backend_ecommerce/backend_ecommerce/models/utils/Roles.java
@@ -3,7 +3,7 @@ package com.backend_ecommerce.backend_ecommerce.models.utils;
 import org.springframework.security.core.GrantedAuthority;
 
 public enum Roles implements GrantedAuthority {
-    ADMIN, USER;
+    ROLE_ADMIN, ROLE_USER;
 
     @Override
     public String getAuthority() {
